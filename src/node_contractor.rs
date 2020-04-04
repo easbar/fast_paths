18e8e9e68ca8a52adf635aaff57c803287b30f4d
easbar <easbar.mail@posteo.net>
2020-04-04 11:08:21 +0200

    No avoid node, no sorting, with path checking

--- src/node_contractor.rs
+++ src/node_contractor.rs
@@ -22,8 +22,6 @@ use crate::constants::Weight;
 use crate::dijkstra::Dijkstra;
 use crate::fast_graph_builder::Params;
 use crate::preparation_graph::PreparationGraph;
-use std::collections::BTreeSet;
-use std::collections::BinaryHeap;
 
 /// removes all edges incident to `node` from the graph and adds shortcuts between all neighbors
 /// of `node` such that all shortest paths are preserved
@@ -58,41 +56,17 @@ pub fn handle_shortcuts<F>(
 ) where
     F: FnMut(&mut PreparationGraph, Shortcut),
 {
-    let mut heap =
-        BinaryHeap::with_capacity(graph.in_edges[node].len() + graph.out_edges[node].len());
-    let mut neighbors = BTreeSet::new();
     for i in 0..graph.in_edges[node].len() {
-        let in_node = graph.in_edges[node][i].adj_node;
-        neighbors.insert(in_node);
         for j in 0..graph.out_edges[node].len() {
-            let out_node = graph.out_edges[node][j].adj_node;
-            neighbors.insert(out_node);
-
             let weight = graph.in_edges[node][i].weight + graph.out_edges[node][j].weight;
-            heap.push((weight, (i, j)));
-        }
-    }
-
-    while !heap.is_empty() {
-        let (weight, (i, j)) = heap.pop().unwrap();
-        let in_node = graph.in_edges[node][i].adj_node;
-        let out_node = graph.out_edges[node][j].adj_node;
-        let path = dijkstra.calc_path(graph, in_node, out_node);
-        if path.is_some() {
-            let shortest_path = path.unwrap();
-            let best_weight = shortest_path.get_weight();
-            let nodes = shortest_path.get_nodes();
-            if best_weight == weight {
-                let mut create_shortcut = true;
-                // skip the in and out nodes
-                for path_node in &nodes[1..(nodes.len() - 1)] {
-                    if neighbors.contains(&path_node) {
-                        create_shortcut = false;
-                    }
-                }
-                if create_shortcut {
+            let in_node = graph.in_edges[node][i].adj_node;
+            let out_node = graph.out_edges[node][j].adj_node;
+            let best_path = dijkstra.calc_path(graph, in_node, out_node);
+            match best_path {
+                Some(p) => if p.get_weight() == weight && p.get_nodes()[1] == node  {
                     handle_shortcut(graph, Shortcut::new(in_node, out_node, node, weight))
-                }
+                },
+                None => {}
             }
         }
     }
